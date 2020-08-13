56c42350a0f2dcece6d9711a8d6b272e96205093
Bin Ouyang <binouyang@Bins-MacBook-Pro.local.dhcp.lbl.gov>
2020-08-13 09:24:12 -0700

    Thu Aug 13 09:24:12 PDT 2020

---
+++ _site/assets/js/common.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('a.abstract').click(function() {
+        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+    });
+    $('a.bibtex').click(function() {
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+    });
+});
