6f9fcade9005b81a13cdada27c5973664e4879c0
Hendrik Schwanekamp <hendrik.schwanekamp@gmx.net>
2021-03-20 16:38:19 +0100

    add cookie consent dialog

---
+++ assets/js/analytics.js
@@ -0,0 +1,26 @@
+---
+---
+
+{% if site.enable_google_analytics %}
+    // google analytics
+    var gaScript = document.createElement('script');
+    gaScript.type = 'text/javascript';
+    gaScript.src = 'https://www.googletagmanager.com/gtag/js?id={{ site.google_analytics }}';
+    document.head.appendChild(gaScript);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', '{{ site.google_analytics }}');
+{% endif %}
+
+{% if site.enable_panelbear_analytics %}
+    // Panelbear analytics
+    var paScript = document.createElement('script');
+    paScript.type = 'text/javascript';
+    paScript.src = 'https://cdn.panelbear.com/analytics.js?site={{site.panelbear_analytics}}';
+    document.head.appendChild(paScript);
+
+    window.panelbear = window.panelbear || function() { (window.panelbear.q = window.panelbear.q || []).push(arguments); };
+    panelbear('config', { site: '{{site.panelbear_analytics}}' });
+{% endif %}
