8554e691812c236f15b127d85ac2a1ae115a5df7
Maruan <alshedivat@users.noreply.github.com>
2021-05-30 19:38:24 -0400

    Adds support for image zoom (#296)
    * Add image zoom
    
    * Minor improvements

---
+++ assets/js/zoom.js
@@ -0,0 +1,8 @@
+
+$(document).ready(function() {
+    mediumZoom('[data-zoomable]', {
+        margin: 100,
+        background: getComputedStyle(document.documentElement)
+            .getPropertyValue('--global-bg-color') + 'ee',
+    })
+});
