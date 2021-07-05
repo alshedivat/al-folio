457389db253f2386aac196253fb72e260a2ab349
Maruan Al-Shedivat <maruan@genesistherapeutics.ai>
2021-07-05 11:10:17 -0700

    Update medium-zoom background when switching theme

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,8 +1,8 @@
-
+// Initialize medium zoom.
 $(document).ready(function() {
-    mediumZoom('[data-zoomable]', {
-        margin: 100,
-        background: getComputedStyle(document.documentElement)
-            .getPropertyValue('--global-bg-color') + 'ee',
-    })
+  medium_zoom = mediumZoom('[data-zoomable]', {
+    margin: 100,
+    background: getComputedStyle(document.documentElement)
+        .getPropertyValue('--global-bg-color') + 'ee',  // + 'ee' for trasparency.
+  })
 });
