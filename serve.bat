512b3af5b16a63a4963bba525a58b046dd89fa51
Patrick Kastner <patrick.kastner@gmail.com>
2021-09-09 15:07:21 -0400

    Disable submenu

--- serve.bat
+++ serve.bat
@@ -5,5 +5,5 @@ FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
 REM FOR /f "tokens=*" %i IN ('docker ps -q') DO docker stop %i
 
 docker run --rm -v=%cd%:/srv/jekyll -p 4000:4000 -it jekyll/jekyll /bin/bash 
-jekyll serve --force_polling
 
+jekyll serve --force_polling
