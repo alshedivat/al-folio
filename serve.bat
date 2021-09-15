d0f0683467b208449ed4b105b569f6926fb16837
Patrick Kastner <patrick.kastner@gmail.com>
2021-09-15 10:09:30 -0400

    .

--- serve.bat
+++ serve.bat
@@ -1,5 +1,7 @@
 REM docker run --rm --volume=%cd%:/srv/jekyll -p 4000:4000  jekyll/jekyll jekyll serve --watch --incremental --force_polling
 FOR /f "tokens=*" %%i IN ('docker ps -q') DO docker stop %%i
 REM FOR /f "tokens=*" %i IN ('docker ps -q') DO docker stop %i
-docker run --rm -v=%cd%:/srv/jekyll -p 4000:4000 -it jekyll/jekyll /bin/bash 
-jekyll serve --force_polling --watch
+REM docker run --rm -v=%cd%:/srv/jekyll -p 4000:4000 -it jekyll/jekyll /bin/bash 
+REM jekyll serve --force_polling --watch
+
+docker run --rm -e "JEKYLL_ENV=docker" -v %cd%:/srv/jekyll -p 4000:4000 -it jekyll/jekyll jekyll serve --config  _config.yml,_config.docker.yml
