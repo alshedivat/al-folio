b81815067bf3bad1c0820077cfc93bd3e071197d
Amir Pourmand <pourmand1376@gmail.com>
2022-07-04 05:05:05 +0430

    remove that line

--- bin/dockerhub_run.sh
+++ bin/dockerhub_run.sh
@@ -5,4 +5,3 @@ fi
 docker run --rm -v "$PWD:/srv/jekyll/" -p "8080:8080" \
                     -it amirpourmand/al-folio bundler  \
                     exec jekyll serve --watch --port=8080 --host=0.0.0.0 
-
