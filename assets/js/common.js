314cb7e2e0fbe77da60db1a3a7714102ee7e7148
LeKhanh <hoangkhanhgk7@gmail.com>
2022-08-12 15:43:31 +0700

    first commit to website

--- assets/js/common.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('a.abstract').click(function() {
-        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
-    });
-    $('a.bibtex').click(function() {
-        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
-    });
-    $('a').removeClass('waves-effect waves-light');
-});
