e3ea30983badda9a67fb8db255bf080da9dd7fd4
GitHub Action <41898282+github-actions[bot]@users.noreply.github.com>
2022-09-16 23:27:28 +0000

    profile changes [ci skip]

--- bin/dockerhub_run.sh
+++
@@ -1,7 +0,0 @@
-FILE=Gemfile.lock
-if [ -f "$FILE" ]; then
-    rm $FILE
-fi
-docker run --rm -v "$PWD:/srv/jekyll/" -p "8081:8081" \
-                    -it amirpourmand/al-folio bundler  \
-                    exec jekyll serve --watch --port=8081 --host=0.0.0.0 
