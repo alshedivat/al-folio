16400fdb8c8f6e0ea3241b5d27e934cda6c1f2f5
Shivam <shivamkumarpanda24@gmail.com>
2022-09-16 17:57:10 -0700

    copy master to gh-pages

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,9 @@
-$(document).ready(function(){$("a.abstract").click(function(){$(this).parent().parent().find(".abstract.hidden").toggleClass("open")}),$("a.bibtex").click(function(){$(this).parent().parent().find(".bibtex.hidden").toggleClass("open")}),$("a").removeClass("waves-effect waves-light")});
+$(document).ready(function() {
+    $('a.abstract').click(function() {
+        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+    });
+    $('a.bibtex').click(function() {
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+    });
+    $('a').removeClass('waves-effect waves-light');
+});
--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,1 +1,8 @@
-$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{margin:100,background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})});
+// Initialize medium zoom.
+$(document).ready(function() {
+  medium_zoom = mediumZoom('[data-zoomable]', {
+    margin: 100,
+    background: getComputedStyle(document.documentElement)
+        .getPropertyValue('--global-bg-color') + 'ee',  // + 'ee' for trasparency.
+  })
+});
