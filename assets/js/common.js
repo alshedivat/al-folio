966d792badeb370c28a1f71f48eb4f54a4261bb5
elchorro <szymon.sacher@gmail.com>
2022-12-02 11:45:18 -0500

    song

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,6 +5,9 @@ $(document).ready(function() {
     $('a.coverage').click(function() {
         $(this).parent().parent().find(".coverage.hidden").toggleClass('open');
     });
+    $('a.song').click(function() {
+        $(this).parent().parent().find(".song.hidden").toggleClass('open');
+    });
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
