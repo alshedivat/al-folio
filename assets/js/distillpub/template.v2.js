5b3e7aeef47e34f6c5cac2d15d5796136e03cceb
Akshita Bhagia <akshita23bhagia@gmail.com>
2022-12-12 01:13:10 -0800

    wip

--- assets/js/distillpub/template.v2.js
+++ assets/js/distillpub/template.v2.js
@@ -2109,12 +2109,36 @@ d-appendix > distill-appendix {
 `;
   }
 
+  function bylineTemplateNoAffiliation(frontMatter) {
+    return `
+  <div class="byline grid">
+    <div class="authors-affiliations grid">
+      <h3>Authors</h3>
+      <h3></h3>
+      ${frontMatter.authors.map(author => `
+        <p class="author">
+          ${author.personalURL ? `
+            <a class="name" href="${author.personalURL}">${author.name}</a>` : `
+            <span class="name">${author.name}</span>`}
+        </p>
+      `).join('')}
+    </div>
+    <div>
+      <h3>Published</h3>
+      ${frontMatter.publishedDate ? `
+        <p>${frontMatter.publishedMonth} ${frontMatter.publishedDay}, ${frontMatter.publishedYear}</p> ` : `
+        <p><em>Not published yet.</em></p>`}
+    </div>
+  </div>
+`;
+  }
+
   class Byline extends HTMLElement {
 
     static get is() { return 'd-byline'; }
 
     set frontMatter(frontMatter) {
-      this.innerHTML = bylineTemplate(frontMatter);
+      this.innerHTML = bylineTemplateNoAffiliation(frontMatter);
     }
 
   }
