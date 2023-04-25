1242eb57f1e4adac78c66feff6e97f38b4a69d5d
Stormshadows <63772315+Stormshadows@users.noreply.github.com>
2023-04-25 06:27:31 +0000

    Deploying to gh-pages from @ lishhavas/lishavas.github.io@b2cb3a317a132b0a32fdc59422bb17ff1ec0e7bb 🚀

--- assets/js/copy_code.js
+++ assets/js/copy_code.js
@@ -1,1 +1,36 @@
-var codeBlocks=document.querySelectorAll("pre");codeBlocks.forEach(function(e){if(e.querySelector("pre:not(.lineno)")||e.querySelector("code")){var n=document.createElement("button");n.className="copy",n.type="button",n.ariaLabel="Copy code to clipboard",n.innerText="Copy",n.innerHTML='<i class="fas fa-clipboard"></i>',e.append(n),n.addEventListener("click",function(){if(e.querySelector("pre:not(.lineno)"))var o=e.querySelector("pre:not(.lineno)").innerText.trim();else o=e.querySelector("code").innerText.trim();window.navigator.clipboard.writeText(o),n.innerText="Copied",n.innerHTML='<i class="fas fa-clipboard-check"></i>',setTimeout(function(){n.innerText="Copy",n.innerHTML='<i class="fas fa-clipboard"></i>'},3e3)})}});
+// create element for copy button in code blocks
+var codeBlocks = document.querySelectorAll('pre');
+codeBlocks.forEach(function (codeBlock) {
+  if (codeBlock.querySelector('pre:not(.lineno)') || codeBlock.querySelector('code')) {
+    var copyButton = document.createElement('button');
+    copyButton.className = 'copy';
+    copyButton.type = 'button';
+    copyButton.ariaLabel = 'Copy code to clipboard';
+    copyButton.innerText = 'Copy';
+    copyButton.innerHTML = '<i class="fas fa-clipboard"></i>';
+    codeBlock.append(copyButton);
+
+    // get code from code block and copy to clipboard
+    copyButton.addEventListener('click', function () {
+      // check if code block has line numbers
+      // i.e. `kramdown.syntax_highlighter_opts.block.line_numbers` set to true in _config.yml
+      // or using `jekyll highlight` liquid tag with `linenos` option
+      if (codeBlock.querySelector('pre:not(.lineno)')) {
+        // get code from code block ignoring line numbers
+        var code = codeBlock.querySelector('pre:not(.lineno)').innerText.trim();
+      } else { // if (codeBlock.querySelector('code')) {
+        // get code from code block when line numbers are not displayed
+        var code = codeBlock.querySelector('code').innerText.trim();
+      }
+      window.navigator.clipboard.writeText(code);
+      copyButton.innerText = 'Copied';
+      copyButton.innerHTML = '<i class="fas fa-clipboard-check"></i>';
+      var waitFor = 3000;
+
+      setTimeout(function () {
+        copyButton.innerText = 'Copy';
+        copyButton.innerHTML = '<i class="fas fa-clipboard"></i>';
+      }, waitFor);
+    });
+  }
+});
