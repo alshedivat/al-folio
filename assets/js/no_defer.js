6ec7750c2de98d6d934bc4ac4a60a7a4d2616a6a
George Araujo <george.gcac@gmail.com>
2023-05-14 16:29:32 -0300

    Added table-dark to all classes when theme is dark
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- assets/js/no_defer.js
+++ assets/js/no_defer.js
@@ -1,6 +1,12 @@
 // add bootstrap classes to tables
 $(document).ready(function() {
   $('table').each(function() {
+    if (document.documentElement.getAttribute("data-theme") == "dark") {
+      $(this).addClass('table-dark');
+    } else {
+      $(this).removeClass('table-dark');
+    }
+
     // only select tables that are not inside an element with "news" (about page) or "card" (cv page) class
     if($(this).parents('[class*="news"]').length==0 &&
         $(this).parents('[class*="card"]').length==0 &&
@@ -10,12 +16,6 @@ $(document).ready(function() {
       // add some classes to make the table look better
       // $(this).addClass('table-sm');
       $(this).addClass('table-hover');
-
-      if (document.documentElement.getAttribute("data-theme") == "dark") {
-        $(this).addClass('table-dark');
-      } else {
-        $(this).removeClass('table-dark');
-      }
     }
   })
 });
