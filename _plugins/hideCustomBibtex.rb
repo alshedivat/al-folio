1e1b5e441e5186940a85b08d274acae558b26ee9
Pascal Berrang <git@paberr.net>
2023-05-15 23:48:28 +0100

    Only filter keywords in attributes

--- _plugins/hideCustomBibtex.rb
+++ _plugins/hideCustomBibtex.rb
@@ -4,7 +4,7 @@
 	  keywords = @context.registers[:site].config['filtered_bibtex_keywords']
 
 	  keywords.each do |keyword|
-		input = input.gsub(/^.*#{keyword}.*$\n/, '')
+		input = input.gsub(/^\s*#{keyword}.*$\n/, '')
 	  end
 
       return input
