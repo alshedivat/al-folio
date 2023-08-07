9758c264b2d2420a4de2bf253fb69b8679537b5d
Verónica Pérez <71244136+VeronicaCPerez@users.noreply.github.com>
2023-08-06 21:33:25 -0500

    adding coverage for GTA paper

--- assets/js/common.js
+++ assets/js/common.js
@@ -5,5 +5,9 @@ $(document).ready(function() {
     $('a.bibtex').click(function() {
         $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
+    // Function to handle click event for "Coverage" button
+    $('a.coverage').click(function() {
+        $(this).parent().parent().find(".coverage.hidden").toggleClass('open');
+    });
     $('a').removeClass('waves-effect waves-light');
 });
