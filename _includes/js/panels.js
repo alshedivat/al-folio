ae43da482d33ee317fd6f47b3a68beff90ac5156
d.s <serrano.david.liaca@gmail.com>
2023-08-29 14:55:07 +0200

    nojs

--- _includes/js/panels.js
+++
@@ -1,5 +0,0 @@
-const panels = document.querySelectorAll('.panel');
-
-panels.forEach(panel => panel.addEventListener('click', (e) => { e.currentTarget.classList.toggle('open'); }));
-
-panels.forEach(panel => panel.addEventListener('transitionend', (e) => { e.currentTarget.classList.toggle('open-active'); }));
---
+++ _posts/2023-08-29-Javascrypt.md
@@ -0,0 +1,11 @@
+---
+layout: post
+title: Javascript
+date: 2023-08-28
+description: Javascript basics
+tags: javascript code js
+categories: code
+featured: true
+toc:
+  sidebar: left
+---
