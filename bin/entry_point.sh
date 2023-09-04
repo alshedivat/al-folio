0fa7ca8138b8e9b6481d4cce7d0d331493d54c6f
Amir Pourmand <pourmand1376@gmail.com>
2023-09-04 15:31:40 +0000

    update entry_point

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
 CONFIG_FILE=_config.yml
-/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace" &
-inotifywait -m -e modify,move,create,delete $CONFIG_FILE |
-while read path action file; do
 
-  echo "Change detected to $CONFIG_FILE, restarting Jekyll"
+/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace" 
+
+inotifywait -m -e modify,move,create,delete $CONFIG_FILE | while read path action file; do
+
+  echo "Change detected to $CONFIG_FILE, restarting Jekyll service"
+  
   # Kill old Jekyll process
-  kill $(pgrep -f jekyll)
+  jekyll_pid=$(pgrep -f jekyll)
+  kill -KILL $jekyll_pid
+
   # Start new Jekyll process
-  /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace" &
+  /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace"
+
 done
