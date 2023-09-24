90658e457d2fe347809c8052b573805b73393889
Demi-wlw <88136271+Demi-wlw@users.noreply.github.com>
2023-09-24 13:16:58 +0100

    Revert "upstream merge template repository"

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,3 @@
-// add toggle functionality to abstract and bibtex buttons
 $(document).ready(function() {
     $('a.abstract').click(function() {
         $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
@@ -10,16 +9,3 @@ $(document).ready(function() {
     });
     $('a').removeClass('waves-effect waves-light');
 });
-
-// bootstrap-toc
-$(document).ready(function () {
-    if($('#toc-sidebar').length){
-        var navSelector = "#toc-sidebar";
-        var $myNav = $(navSelector);
-        Toc.init($myNav);
-        $("body").scrollspy({
-            target: navSelector,
-        });
-    }
-});
-
