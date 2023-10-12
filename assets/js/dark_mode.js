5b30fc7090512de4707f2526b033f3c21641ab79
saitzaw <saithihazaw43@gmail.com>
2023-10-12 09:02:13 +0000

    Deploying to gh-pages from @ saitzaw/saithihazaw.io@8d29635196d62aedb47f8e5165809d67e37ebe53 🚀

---
+++ assets/js/dark_mode.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",function(){document.getElementById("light-toggle").addEventListener("click",function(){toggleTheme(localStorage.getItem("theme"))})});
---
+++ assets/js/masonry.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){var r=$(".grid").masonry({gutter:10,horizontalOrder:!0,itemSelector:".grid-item"});r.imagesLoaded().progress(function(){r.masonry("layout")})});
