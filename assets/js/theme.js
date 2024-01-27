b425b7757f65c9f502554e007c36f75a7d5adeea
George Araújo <george.gcac@gmail.com>
2024-01-27 01:18:33 -0300

    Fixed prettier complaints
    Signed-off-by: George Araújo <george.gcac@gmail.com>

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -129,8 +129,8 @@ let setVegaLiteTheme = (theme) => {
     // Get the code block content from previous element, since it is the vega lite code itself as defined in Markdown, but it is hidden
     let jsonData = elem.previousSibling.childNodes[0].innerHTML;
     elem.innerHTML = "";
-    if (theme === 'dark') {
-      vegaEmbed(elem, JSON.parse(jsonData), {theme: 'dark'});
+    if (theme === "dark") {
+      vegaEmbed(elem, JSON.parse(jsonData), { theme: "dark" });
     } else {
       vegaEmbed(elem, JSON.parse(jsonData));
     }
