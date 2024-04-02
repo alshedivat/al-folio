85dd233649ff9fcb95c0638037cf8a883cc0e9e3
George Araujo <george.gcac@gmail.com>
2024-04-01 23:22:36 -0300

    Downloading also fonts from 3rd parties when explicited
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- _plugins/download-3rd-party.rb
+++ _plugins/download-3rd-party.rb
@@ -1,7 +1,9 @@
 Jekyll::Hooks.register :site, :after_init do |site|
   require 'digest'
   require 'fileutils'
+  require 'nokogiri'
   require 'open-uri'
+  require 'uri'
 
   def download_file(url, dest)
     # create the directory if it doesn't exist
@@ -22,6 +24,21 @@ Jekyll::Hooks.register :site, :after_init do |site|
       # check if the file was downloaded successfully
       unless File.file?(dest)
         raise "Failed to download #{url} to #{dest}"
+      end
+    end
+  end
+
+  def download_fonts(url, dest)
+    # get available fonts from the url
+    doc = Nokogiri::HTML(URI.open(url, "User-Agent" => "Ruby/#{RUBY_VERSION}"))
+    doc.css('a').each do |link|
+      # get the file name from the url
+      file_name = link['href'].split('/').last.split('?').first
+
+      # verify if the file is a font file
+      if file_name.end_with?('.woff', '.woff2', '.ttf', '.otf')
+        # download the file and change the url to the local file
+        download_file(URI.join(url, link['href']).to_s, File.join(dest, file_name))
       end
     end
   end
@@ -57,23 +74,27 @@ Jekyll::Hooks.register :site, :after_init do |site|
               dest = File.join(site.source, 'assets', 'libs', file_name)
               download_file(url, dest)
               # change the url to the local file, considering baseurl
-            if site.config['baseurl']
-              site.config['third_party_libraries'][key]['url'][type][type2] = File.join(site.config['baseurl'], 'assets', 'libs', file_name)
-            else
-              site.config['third_party_libraries'][key]['url'][type][type2] = File.join('/assets', 'libs', file_name)
-            end
+              if site.config['baseurl']
+                site.config['third_party_libraries'][key]['url'][type][type2] = File.join(site.config['baseurl'], 'assets', 'libs', file_name)
+              else
+                site.config['third_party_libraries'][key]['url'][type][type2] = File.join('/assets', 'libs', file_name)
+              end
             end
           else
-            # get the file name from the url
-            file_name = url.split('/').last.split('?').first
-            # download the file and change the url to the local file
-            dest = File.join(site.source, 'assets', 'libs', file_name)
-            download_file(url, dest)
-            # change the url to the local file, considering baseurl
-            if site.config['baseurl']
-              site.config['third_party_libraries'][key]['url'][type] = File.join(site.config['baseurl'], 'assets', 'libs', file_name)
+            if type == 'fonts'
+              download_fonts(url, File.join(site.source, 'assets', 'libs', site.config['third_party_libraries'][key]['local']['fonts']))
             else
-              site.config['third_party_libraries'][key]['url'][type] = File.join('/assets', 'libs', file_name)
+              # get the file name from the url
+              file_name = url.split('/').last.split('?').first
+              # download the file and change the url to the local file
+              dest = File.join(site.source, 'assets', 'libs', file_name)
+              download_file(url, dest)
+              # change the url to the local file, considering baseurl
+              if site.config['baseurl']
+                site.config['third_party_libraries'][key]['url'][type] = File.join(site.config['baseurl'], 'assets', 'libs', file_name)
+              else
+                site.config['third_party_libraries'][key]['url'][type] = File.join('/assets', 'libs', file_name)
+              end
             end
           end
         end
