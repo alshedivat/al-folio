94e39119ecea9afcfa1129e3265fc32273f6c3f8
Andreas Säuberli <andreas.saeuberli@gmail.com>
2024-04-11 14:09:58 +0200

    Disable Sass compression in dev setup

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 CONFIG_FILE=_config.yml 
+CONFIG_FILE_DEV=_config-dev.yml 
 
-/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+/bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --config=$CONFIG_FILE,$CONFIG_FILE_DEV --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
 while true; do
 
@@ -10,12 +11,12 @@ while true; do
 
   if [ $? -eq 0 ]; then
  
-    echo "Change detected to $CONFIG_FILE, restarting Jekyll"
+    echo "Change detected to $CONFIG_FILE or $CONFIG_FILE_DEV, restarting Jekyll"
 
     jekyll_pid=$(pgrep -f jekyll)
     kill -KILL $jekyll_pid
 
-    /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
+    /bin/bash -c "rm -f Gemfile.lock && exec jekyll serve --config=$CONFIG_FILE,$CONFIG_FILE_DEV --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling"&
 
   fi
 
