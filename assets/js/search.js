c78ef6e2f0e772e7b3d63889a7102361f7604ca4
George Araujo <george.gcac@gmail.com>
2024-05-13 15:01:25 -0300

    Now adding all posts and pages in main search
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- assets/js/search.js
+++ assets/js/search.js
@@ -3,98 +3,79 @@
 // don't remove the above lines
 // they are required to make the code work
 
-// Get list of posts from Jekyll
-const posts = [
-  {% for post in site.posts %}
-    {
-      title: "{{ post.title | escape }}",
-      description: "{{ post.description | strip_html | strip_newlines | escape }}",
-      url: "{{ site.baseurl }}{{ post.url }}",
-      date: "{{ post.date | date: "%Y-%m-%d" }}",
-      categories: [
-        {% for category in post.categories %}
-          "{{ category }}",
-        {% endfor %}
-      ],
-      tags: [
-        {% for tag in post.tags %}
-          "{{ tag }}",
-        {% endfor %}
-      ]
-    },
-  {% endfor %}
-];
-
-const projects = [
-  {% for project in site.projects %}
-    {
-      title: "{{ project.title | escape }}",
-      description: "{{ post.description | strip_html | strip_newlines | escape }}",
-      url: "{{ site.baseurl }}{{ project.url }}",
-      categories: [
-        "{{ project.category }}",
-      ]
-    },
-  {% endfor %}
-];
-
-// Custom Icons for Home and Posts Menu Items
-// You can use any SVG icon you want
-const articleIcon = '<svg style="padding-right: 10px" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 22H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1zM7 6v4h4V6H7zm0 6v2h10v-2H7zm0 4v2h10v-2H7zm6-9v2h4V7h-4z" fill="#fff"/></svg>';
-const homeIcon = '<svg style="padding-right: 10px" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M13.376 3.47a2.248 2.248 0 0 0-2.752 0l-6.75 5.222A2.254 2.254 0 0 0 3 10.475v8.272A2.251 2.251 0 0 0 5.25 21h3a2.251 2.251 0 0 0 2.25-2.253v-2.252a1.5 1.5 0 1 1 3 0v2.252A2.251 2.251 0 0 0 15.75 21h3A2.251 2.251 0 0 0 21 18.747v-8.272c0-.698-.323-1.356-.874-1.783l-6.75-5.222Z" fill="#fff"/></svg>';
-const projectIcon = '<svg style="padding-right: 10px" width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path fill="none" d="M0 0h24v24H0z"/><path d="M20 22H4a1 1 0 0 1-1-1V3a1 1 0 0 1 1-1h16a1 1 0 0 1 1 1v18a1 1 0 0 1-1 1zM7 6v4h4V6H7zm0 6v2h10v-2H7zm0 4v2h10v-2H7zm6-9v2h4V7h-4z" fill="#fff"/></svg>';
-
 // get the ninja-keys element
 const ninja = document.querySelector('ninja-keys');
 
-// map the posts to the ninja-keys data format
-const myPosts =  posts.map(post => {
-  return {
-    id: post.title,
-    title: post.title,
-    content: post.content,
-      icon: articleIcon,
-      parent: 'posts',
-      handler: () => {
-        window.location.href = post.url;
-      },
-  };
-});
-
-// map the projects to the ninja-keys data format
-const myProjects =  projects.map(project => {
-  return {
-    id: project.title,
-      title: project.title,
-      content: project.content,
-      icon: projectIcon,
-      parent: 'projects',
-      handler: () => {
-        window.location.href = project.url;
-      },
-  };
-});
-
 // add the home and posts menu items
 ninja.data = [
+  {% for page in site.pages %}
+    {% if page.permalink == '/' %} {% assign about_title = page.title %} {% endif %}
+  {% endfor %}
   {
-    id: 'home',
-    title: 'Home',
-    icon: homeIcon,
+    id: "nav-{{ about_title | slugify }}",
+    title: "{{ about_title }}",
+    section: "Navigation",
     handler: () => {
-      window.location.href = "{{ site.baseurl }}/";
+      window.location.href = "{{ '/' | relative_url }}";
     },
   },
-  {
-    id: 'posts',
-    title: 'Posts',
-    icon: articleIcon,
-    children: myPosts,
-  },
-  {
-    id: 'projects',
-    title: 'Projects',
-    icon: projectIcon,
-    children: myProjects,
-  }
+  {% assign sorted_pages = site.pages | sort: "nav_order" %}
+  {% for p in sorted_pages %}
+    {% if p.nav and p.autogen == null %}
+      {% if p.dropdown %}
+        {% for child in p.children %}
+          {% unless child.title == 'divider' %}
+            {
+              {% assign title = child.title | escape %}
+              {% if child.permalink contains "/blog/" %}{% assign url = "/blog/" %} {% else %}{% assign url = child.url %}{% endif %}
+              id: "dropdown-{{ title | slugify }}",
+              title: "{{ title }}",
+              description: "{{ child.description | strip_html | strip_newlines | escape }}",
+              section: "Dropdown",
+              handler: () => {
+                window.location.href = "{{ url | relative_url }}";
+              },
+            },
+          {% endunless %}
+        {% endfor %}
+
+      {% else %}
+        {
+          {% assign title = p.title | escape %}
+          {% if p.permalink contains "/blog/" %}{% assign url = "/blog/" %} {% else %}{% assign url = p.url %}{% endif %}
+          id: "nav-{{ title | slugify }}",
+          title: "{{ title }}",
+          description: "{{ p.description | strip_html | strip_newlines | escape }}",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "{{ url | relative_url }}";
+          },
+        },
+      {% endif %}
+    {% endif %}
+  {% endfor %}
+  {% for post in site.posts %}
+    {
+      {% assign title = post.title | escape %}
+      id: "post-{{ title | slugify }}",
+      title: "{{ title }}",
+      description: "{{ post.description | strip_html | strip_newlines | escape }}",
+      section: "Posts",
+      handler: () => {
+        window.location.href = "{{ post.url | relative_url }}";
+      },
+    },
+  {% endfor %}
+  {% for project in site.projects %}
+    {
+      {% assign title = project.title | escape %}
+      id: "project-{{ title | slugify }}",
+      title: "{{ title }}",
+      description: "{{ project.description | strip_html | strip_newlines | escape }}",
+      section: "Projects",
+      handler: () => {
+        window.location.href = "{{ project.url | relative_url }}";
+      },
+    },
+  {% endfor %}
 ];
