ce395f0b25be397776956a2ec7200b4ff259f97b
George Araujo <george.gcac@gmail.com>
2024-05-17 11:35:35 -0300

    Renamed search js files to min
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- assets/js/search/lit/decorators.js
+++
@@ -1,8 +0,0 @@
-/**
- * Bundled by jsDelivr using Rollup v2.79.1 and Terser v5.19.2.
- * Original file: /npm/lit@3.1.3/decorators.js
- *
- * Do NOT use SRI with dynamically generated files! More information: https://www.jsdelivr.com/using-sri-with-dynamic-files
- */
-export*from"../reactive-element/decorators/custom-element.js";export*from"../reactive-element/decorators/property.js";export*from"../reactive-element/decorators/state.js";export*from"../reactive-element/decorators/event-options.js";export*from"../reactive-element/decorators/query.js";export*from"../reactive-element/decorators/query-all.js";export*from"../reactive-element/decorators/query-async.js";export*from"../reactive-element/decorators/query-assigned-elements.js";export*from"../reactive-element/decorators/query-assigned-nodes.js";export default null;
-//# sourceMappingURL=/sm/78d9c5b63a95c0cca76c523f4d858b830d1266d442a34d7b53ea198d16f442d6.map
