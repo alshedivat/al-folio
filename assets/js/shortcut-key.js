12b476eb7c98981a0b3fd3f536c95d9a4d4cceb7
Andrew Leonard <ajyey@andrewleonard.dev>
2024-05-29 01:18:27 -0400

    Dynamically sets the search shortcut key based on platform
    This addresses issue #2437

---
+++ assets/js/shortcut-key.js
@@ -0,0 +1,12 @@
+// Check if the user is on a Mac and update the shortcut key for search accordingly
+
+var isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+document.addEventListener('readystatechange', function () {
+  if (document.readyState === 'interactive') {
+    var shortcutKeyElement = document.querySelector('#search-toggle .nav-link');
+    if (shortcutKeyElement && isMac) {
+        // use the unicode for command key
+        shortcutKeyElement.innerHTML = "&#x2318; k <i class='ti ti-search'></i>";
+    }
+  }
+});
