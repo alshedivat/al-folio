e66525d06cee78f5ee07534d9fa85352614a77c6
Andrew Leonard <ajyey@andrewleonard.dev>
2024-05-31 12:25:02 -0400

    fixes prettier errors

--- assets/js/shortcut-key.js
+++ assets/js/shortcut-key.js
@@ -1,12 +1,12 @@
 // Check if the user is on a Mac and update the shortcut key for search accordingly
 (function () {
-  let isMac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
-  document.addEventListener('readystatechange', function () {
-    if (document.readyState === 'interactive') {
-      let shortcutKeyElement = document.querySelector('#search-toggle .nav-link');
+  let isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
+  document.addEventListener("readystatechange", function () {
+    if (document.readyState === "interactive") {
+      let shortcutKeyElement = document.querySelector("#search-toggle .nav-link");
       if (shortcutKeyElement && isMac) {
-          // use the unicode for command key
-          shortcutKeyElement.innerHTML = "&#x2318; k <i class='ti ti-search'></i>";
+        // use the unicode for command key
+        shortcutKeyElement.innerHTML = "&#x2318; k <i class='ti ti-search'></i>";
       }
     }
   });
