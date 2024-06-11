173f4fc6a9d5a0f367f444d00aa72f3fa9c1074f
George Araujo <george.gcac@gmail.com>
2024-06-11 13:47:44 -0300

    Replaced all document loaded checks
    Signed-off-by: George Araujo <george.gcac@gmail.com>

--- assets/js/shortcut-key.js
+++ assets/js/shortcut-key.js
@@ -1,5 +1,5 @@
 // Check if the user is on a Mac and update the shortcut key for search accordingly
-document.onreadystatechange = () => {
+document.addEventListener("readystatechange", () => {
   if (document.readyState === "interactive") {
     let isMac = navigator.platform.toUpperCase().indexOf("MAC") >= 0;
     let shortcutKeyElement = document.querySelector("#search-toggle .nav-link");
@@ -8,4 +8,4 @@ document.onreadystatechange = () => {
       shortcutKeyElement.innerHTML = '&#x2318; k <i class="ti ti-search"></i>';
     }
   }
-};
+});
