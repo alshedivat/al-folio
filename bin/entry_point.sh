c0d53e631630b19328f30f9e0da900ff1161eb27
Amir Pourmand <pourmand1376@gmail.com>
2024-10-02 10:21:52 +0330

    Change Run to use bundle exec instead of normal exec jekyll

--- bin/entry_point.sh
+++ bin/entry_point.sh
@@ -18,7 +18,7 @@ manage_gemfile_lock() {
 
 start_jekyll() {
     manage_gemfile_lock
-    exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling &
+    bundle exec jekyll serve --watch --port=8080 --host=0.0.0.0 --livereload --verbose --trace --force_polling &
 }
 
 start_jekyll
