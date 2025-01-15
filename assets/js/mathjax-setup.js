ee1d330ed46aa8d901876c833f81d648295fe8b0
Suraj Khamkar <surajk@atlanticpkg.com>
2025-01-15 21:49:17 +0000

    Fix issue causing no color inversion when rendering equations

--- assets/js/mathjax-setup.js
+++ assets/js/mathjax-setup.js
@@ -1,5 +1,26 @@
 window.MathJax = {
   tex: {
     tags: "ams",
+    inlineMath: [
+      ["$", "$"],
+      ["\\(", "\\)"],
+    ],
+  },
+  options: {
+    renderActions: {
+      addCss: [
+        200,
+        function (doc) {
+          const style = document.createElement("style");
+          style.innerHTML = `
+          .mjx-container {
+            color: inherit;
+          }
+        `;
+          document.head.appendChild(style);
+        },
+        "",
+      ],
+    },
   },
 };
