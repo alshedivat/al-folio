fb692a21a627aff71a34dfbe74a25de67f4e0ef8
RyanCaldwell360 <ryancaldwell918@gmail.com>
2025-03-30 22:11:33 -0500

    cleaned up files

--- assets/js/cronitor-analytics-setup.js
+++
@@ -1,6 +0,0 @@
-window.cronitor =
-  window.cronitor ||
-  function () {
-    (window.cronitor.q = window.cronitor.q || []).push(arguments);
-  };
-cronitor("config", { clientKey: "{{ site.cronitor_analytics }}" });
