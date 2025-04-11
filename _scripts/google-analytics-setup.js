266a0b566caab3611b6c97d0eff636f08bc82be6
XinLIU56 <106716317+XinLIU56@users.noreply.github.com>
2025-04-11 02:52:42 +0000

    Deploying to gh-pages from @ XinLIU56/xinliu56.github.io@6e1a803c84c42a4e8f2965b483f99770392869b6 🚀

--- _scripts/google-analytics-setup.js
+++
@@ -1,9 +0,0 @@
----
-permalink: /assets/js/google-analytics-setup.js
----
-window.dataLayer = window.dataLayer || [];
-function gtag() {
-  window.dataLayer.push(arguments);
-}
-gtag("js", new Date());
-gtag("config", "{{ site.google_analytics }}");
