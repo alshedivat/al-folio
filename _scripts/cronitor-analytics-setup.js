276b9b0bf3ebb0a13eb66c60924df8fc2f6421a6
Kaeptenbalu <bollmanuel@gmail.com>
2025-07-16 20:11:27 +0200

    ewf [ci skip]

--- _scripts/cronitor-analytics-setup.js
+++
@@ -1,9 +0,0 @@
----
-permalink: /assets/js/cronitor-analytics-setup.js
----
-window.cronitor =
-  window.cronitor ||
-  function () {
-    (window.cronitor.q = window.cronitor.q || []).push(arguments);
-  };
-cronitor("config", { clientKey: "{{ site.cronitor_analytics }}" });
---
+++ assets/js/search-data.js
@@ -0,0 +1,71 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-about",
+    title: "about",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "news-brilliant-and-simple-filename-based-sandbox-evasion",
+          title: 'Brilliant and Simple - Filename-Based Sandbox Evasion',
+          description: "",
+          section: "News",handler: () => {
+              window.location.href = "/news/announcement_1/";
+            },},{
+        id: 'social-email',
+        title: 'email',
+        section: 'Socials',
+        handler: () => {
+          window.open("mailto:%68%65%6C%6C%6F@%6D%62%6F%6C%6C.%65%75", "_blank");
+        },
+      },{
+        id: 'social-github',
+        title: 'GitHub',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://github.com/kaeptenbalu", "_blank");
+        },
+      },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/manuel-boll-78097b217/", "_blank");
+        },
+      },{
+        id: 'social-custom_social',
+        title: 'Custom_social',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://ctftime.org/user/82511", "_blank");
+        },
+      },{
+      id: 'light-theme',
+      title: 'Change theme to light',
+      description: 'Change the theme of the site to Light',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("light");
+      },
+    },
+    {
+      id: 'dark-theme',
+      title: 'Change theme to dark',
+      description: 'Change the theme of the site to Dark',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("dark");
+      },
+    },
+    {
+      id: 'system-theme',
+      title: 'Use system default theme',
+      description: 'Change the theme of the site to System Default',
+      section: 'Theme',
+      handler: () => {
+        setThemeSetting("system");
+      },
+    },];
