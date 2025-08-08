2d80ce415683a90f6d9ba5ff9a277d963121164e
Ariel Goodwin <46231728+arielgoodwin@users.noreply.github.com>
2025-08-08 15:49:44 -0400

    Delete assets directory

--- assets/js/masonry.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){var r=$(".grid").masonry({gutter:10,horizontalOrder:!0,itemSelector:".grid-item"});r.imagesLoaded().progress(function(){r.masonry("layout")})});
--- assets/js/zoom.js
+++
@@ -1,1 +0,0 @@
-$(document).ready(function(){medium_zoom=mediumZoom("[data-zoomable]",{margin:100,background:getComputedStyle(document.documentElement).getPropertyValue("--global-bg-color")+"ee"})});
