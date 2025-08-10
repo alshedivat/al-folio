9a0c7569b8cce20f4dad1c1e7c7f94b4d39b9678
kaeptenbalu <boll.manuel@gmail.com>
2025-08-10 06:13:07 +0000

    Deploying to gh-pages from @ kaeptenbalu/kaeptenbalu.github.io@0d77f0a96b4d57ca7e8a58024bbe2a184f91561a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,12 +18,12 @@ ninja.data = [{
           title: 'Slices of Suspicion – The Pentagon Pizza Theory',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_1/";
+              window.location.href = "/news/announcement_7/";
             },},{id: "news-brilliant-and-simple-filename-based-sandbox-evasion",
           title: 'Brilliant and Simple - Filename-Based Sandbox Evasion',
           description: "",
           section: "News",handler: () => {
-              window.location.href = "/news/announcement_2/";
+              window.location.href = "/news/announcement_1/";
             },},{id: "news-a-promptly-bad-idea-malware-meets-ai",
           title: 'A Promptly Bad Idea - Malware Meets AI',
           description: "",
