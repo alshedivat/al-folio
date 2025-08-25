cf810c83f4dd401715220677d1a3517b42ad7792
george-gca <31376482+george-gca@users.noreply.github.com>
2025-08-25 21:23:21 +0000

    Deploying to gh-pages from @ alshedivat/al-folio@cf8e02f98bc76d082850ffdfefb346cd53eef10e 🚀

---
+++ assets/js/giscus-setup.js
@@ -0,0 +1,42 @@
+function determineGiscusTheme() {
+  
+    let theme =
+      localStorage.getItem("theme") ||
+      document.documentElement.getAttribute("data-theme") ||
+      "system";
+
+    if (theme === "dark") return "dark";
+    if (theme === "light") return "light";
+
+    const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    return prefersDark ? "dark" : "light";
+  
+}
+
+(function setupGiscus() {
+  let giscusTheme = determineGiscusTheme();
+
+  let giscusAttributes = {
+    src: "https://giscus.app/client.js",
+    "data-repo": "alshedivat/al-folio",
+    "data-repo-id": "",
+    "data-category": "Comments",
+    "data-category-id": "",
+    "data-mapping": "title",
+    "data-strict": "1",
+    "data-reactions-enabled": "1",
+    "data-emit-metadata": "0",
+    "data-input-position": "bottom",
+    "data-theme": giscusTheme,
+    "data-lang": "en",
+    crossorigin: "anonymous",
+    async: true,
+  };
+
+  let giscusScript = document.createElement("script");
+  Object.entries(giscusAttributes).forEach(([key, value]) =>
+    giscusScript.setAttribute(key, value)
+  );
+  document.getElementById("giscus_thread").appendChild(giscusScript);
+})();
+
