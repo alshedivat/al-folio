416a4a367a41abe8dc0481d9f5bb006190736d54
kaeptenbalu <boll.manuel@gmail.com>
2025-08-31 08:00:39 +0000

    Deploying to gh-pages from @ kaeptenbalu/kaeptenbalu.github.io@e0f21c4c4aa1ea7decccb9926b529e6e27892f90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-home",
+    title: "home",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
