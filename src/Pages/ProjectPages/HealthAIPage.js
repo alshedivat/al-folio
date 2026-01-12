84615db456857037e88b658383b091196788cc1c
Joonyoung Park <joonyoung@kaist.ac.kr>
2026-01-12 12:36:30 +0900

    [refactor] Update projects and publications

--- src/Pages/ProjectPages/HealthAIPage.js
+++ src/Pages/ProjectPages/HealthAIPage.js
@@ -9,6 +9,6 @@ export const HealthAIPage = (props) => (
     description={
       "We build and evaluate the next generation of interactive systems for mental health and well-being. We move beyond simple tracking, utilizing generative AI and novel interaction paradigms to create tools for human-AI co-creation in therapeutic journaling, data-driven self-reflection, and designing new approaches to understanding personal data."
     }
-    field={"health-ai"}
+    field={"health"}
   />
 );
