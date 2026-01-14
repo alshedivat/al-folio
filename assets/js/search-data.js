6887cfaad4c552bed502ab28d9e0261b87d050d6
george-gca <31376482+george-gca@users.noreply.github.com>
2026-01-14 16:21:06 +0000

    Deploying to gh-pages from @ alshedivat/al-folio@17b0251c0f8a7380c7c59cff4f57c34f6bafe511 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -325,6 +325,17 @@ ninja.data = [{
             window.location.href = "/al-folio/blog/2022/giscus-comments/";
           
         },
+      },{id: "post-displaying-external-posts-on-your-al-folio-blog",
+        
+          title: 'Displaying External Posts on Your al-folio Blog <svg width="1.2rem" height="1.2rem" top=".5rem" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M17 13.5v6H5v-12h6m3-3h6v6m0-6-9 9" class="icon_svg-stroke" stroke="#999" stroke-width="1.5" fill="none" fill-rule="evenodd" stroke-linecap="round" stroke-linejoin="round"></path></svg>',
+        
+        description: "",
+        section: "Posts",
+        handler: () => {
+          
+            window.open("https://medium.com/@al-folio/displaying-external-posts-on-your-al-folio-blog-b60a1d241a0a?source=rss-17feae71c3c4------2", "_blank");
+          
+        },
       },{id: "post-a-post-with-redirect",
         
           title: "a post with redirect",
